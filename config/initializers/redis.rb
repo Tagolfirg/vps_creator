d6b4aeedf3c679a5f7de515cd9bb5f70e1f5721a
jet <jet@chinook.homenet>
2015-05-18 12:44:24 -0700

    temp commenting out of apppliction_controller.rb, application.html.erb, and the intitializer redis.rb

---
+++ config/initializers/redis.rb
@@ -0,0 +1,7 @@
+# config/initializers/redis.rb
+### BELOW TEMPORARILY COMMENTED OUT WHILE TESTING #############
+#
+
+# location = ENV["REDISCLOUD_URL"] || 'redis://127.0.0.10:6379/0'
+# uri = URI.parse(location)
+# $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
