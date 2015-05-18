8f98f374e28dee55648b56ae89ac1e2e89607308
jet <jet@chinook.homenet>
2015-05-18 11:17:17 -0700

    completion of base recipes: users, ssh, redis rbenv, postgres, nginx, app

---
+++ config/chef/site-cookbooks/worldmaker/attributes/default.rb
@@ -0,0 +1,7 @@
+default['app']               = 'worldmaker'
+
+default['nodejs']['dir']     = '/usr/local'
+default['nodejs']['version'] = '0.12.3'
+
+default['ruby']['version']   = '2.2.2'
+default['redis']['version']  = '3.0.1'
