d62b39ee44be9525f7d458d05455738ac8b10d4e
Jerry Thompson <jerrold.r.thompson@gmail.com>
2015-05-20 11:13:59 -0700

    Attempting to use script to install Redis--untested

--- config/chef/site-cookbooks/worldmaker/README.md
+++ config/chef/site-cookbooks/worldmaker/README.md
@@ -1,9 +1,6 @@
 worldmaker Cookbook
 ===================
-TODO: Enter the cookbook description here.
 
-e.g.
-This cookbook makes your favorite breakfast sandwich.
 
 Requirements
 ------------
--- config/chef/site-cookbooks/worldmaker/recipes/redis.rb
+++ config/chef/site-cookbooks/worldmaker/recipes/redis.rb
@@ -20,6 +20,21 @@ bash 'install redis' do
 end
 
 # install redis server
+
+
+cookbook_file "redis_server_installer.sh" do
+  source "redis_server_installer.sh"
+  mode 0755
+end
+
+# jet below has not been tested, 20May2015
+execute "Install redis server" do
+  command "sh redis_server_installer.sh"
+  cwd "/home/#{node['user']['name']}/redis-#{node['redis']['version']}/utils"
+  not_if "ls /etc/init.d | grep redis"
+end
+
+# jet Below was working, but commented out to test above
 execute "curl -L https://gist.githubusercontent.com/vladigleba/28f4f6b4454947c5223e/raw | sh" do
   cwd "/home/#{node['user']['name']}/redis-#{node['redis']['version']}/utils"
   not_if "ls /etc/init.d | grep redis"
